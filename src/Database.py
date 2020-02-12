33a3fd490d613433609e232f9d2c01e7c8ebd591
flanders <flanders@flanders>
2020-02-12 10:49:20 -0600

    Add database wrapper

--- src/Database.py
+++ src/Database.py
@@ -1,5 +1,13 @@
 import pymysql.cursors
 
-class Database
-    def __init__(self):
-        pass
+class Database:
+    def __init__(self, db, user, password):
+        self.connection = pymysql.connect(host='localhost',
+                                          user=user,
+                                          password=password,
+                                          db=db,
+                                          cursorclass=pymysql.cursors.DictCursor)
+        self.cursor = self.connection.cursor()
+
+    def query(self, query_string):
+        return self.cursor.execute(query_string)
