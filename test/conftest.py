d56a50bb8b257e63c61e3dafb8d1d132b02935a0
flanders <flanders@flanders>
2020-02-12 12:19:37 -0600

    working on test setup fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -5,10 +5,28 @@ from Database import Database
 
 @pytest.fixture
 def with_database():
+    repo_id = create_repository()
+    create_file(repo_id, "one.r")
+    create_file(repo_id, "two.r")
+    yield
+    teardown()
+
+def create_repository():
     db = Database("test", "root", "")
     db.query("insert into Repositories (path) values ('.');")
+    repo = db.query("select * from Repositories limit 1")[0]
+    return repo['repositoryID']
 
-    yield
+def create_file(repo_id, name):
+    db = Database("test", "root", "")
+    query_string = "insert into RFiles (repositoryID, \
+            filePath, fileType) values ({}, '{}', {});".format(repo_id, name, 1)
 
+    db.query(query_string)
+
+
+def teardown():
+    db = Database("test", "root", "")
+    db.query("delete from RFiles where 1=1;")
     db.query("delete from Repositories where 1=1;")
 
--- test/source_to_test_test.py
+++ test/source_to_test_test.py
@@ -5,5 +5,3 @@ from Database import Database
 
 def test_gets_the_tests(with_database):
     db = Database("test","root","")
-    results = db.query("select * from Repositories;")
-    print(results)
