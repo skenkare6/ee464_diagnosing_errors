88c0a1de8ff981aea7b3ceda0e63a5bdc85b94bb
John Koelling <john.koelling@utexas.edu>
2020-02-23 17:05:49 -0600

    Added .R filetype check
    Also using GIT_ROOT variable which may be removed in the future

--- sandbox/Module2/wrapper.sh
+++ sandbox/Module2/wrapper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-. DiffLinesFunction.sh | while read entry
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+. DiffLinesFunction.sh | grep *\.[rR]$ | while read entry
 do
-Rscript contextLineNumber.R $entry
+Rscript contextLineNumber.R ${GIT_ROOT}/${entry}
 done
