edd3441eddd8695943f2072fe982caea9a56c11b
flanders <flanders@flanders>
2020-03-04 12:17:07 -0600

    Fix conftest file after moving SourceToTest module

--- test/database_managers_test/source_to_test_test.py
+++ test/database_managers_test/source_to_test_test.py
@@ -4,6 +4,7 @@ from SourceFile import SourceFile
 from Database import Database
 
 import SourceToTest
+
 def test_gets_json(with_database):
   json_report = json.loads(SourceToTest.getJson("one.r"))
   assert set(json_report.keys()) == set(['one.r'])
